cb92eff0e7d2f3fe21977ba62c4cd5516dfb62bf
Kumar Daryanani <destral@msn.com>
2024-02-07 16:55:59 +0100

    Day 3 - create and add new app pages

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,27 @@
+function AboutPage() {
+  return (
+    <>
+      <h1>About Page</h1>
+      <h2>Project Name</h2>
+      <h3>by team Malumar</h3>
+      <p>
+        Project description goes here and is very awesome and you should read it
+        because if you don't you are not awesome and you will make us very sad.
+      </p>
+      <div>
+        <h2>Team Members</h2>
+        <div>
+          <h3>Team member 1</h3>
+          <a href="#">linkedin</a>
+          <a href="">github</a>
+        </div>
+        <div>
+          <h3>Team member 2</h3>
+          <a href="#">linkedin</a>
+          <a href="">github</a>
+        </div>
+      </div>
+    </>
+  );
+}
+export default AboutPage;
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+function NotFoundPage() {
+    return(
+        <>
+        <h1>404 Not Found</h1>
+        <p>You seem to be very lost...</p>
+        <p>Please go back somewhere <a href="/">safe</a></p>
+        </>
+    )
+}
+export default NotFoundPage;
