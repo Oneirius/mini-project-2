abd07cd460cbde5eaaf2055aeb98b3cebd4489f9
Kumar Daryanani <destral@msn.com>
2024-02-14 23:37:48 +0100

    Styles, Layout, Delete functionality

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,22 +4,23 @@ import TaskCard from "./TaskCard.jsx";
 import "./List.css"
 
 function List(props) {
-    const database = props.database
+    const database = props.database;
+    const setDatabase = props.setDatabase;
 
 
     function deleteTask (taskID){
-        const newTaskList = taskArray.filter((ele)=>{
+        const newTaskList = database.filter((ele)=>{
             return taskID !== ele.id 
         })
         console.log(newTaskList);
-        setTasks(newTaskList);
+        setDatabase(newTaskList);
     }
 
     return(
         <ul>
             {
             database.map((task)=>{
-                return <TaskCard key={task.id} task={task} deleteTask={deleteTask}/>
+                return <TaskCard key={task.id} task={task} deleteTask={deleteTask} setDatabase={setDatabase}/>
                 
             })
             }
--- src/pages/EditTaskPage.jsx
+++ src/pages/EditTaskPage.jsx
@@ -1,9 +1,166 @@
-function EditPage() {
-    return(
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
+function EditTaskPage(props) {
+
+  const [id, setId] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [assignee, setAssignee] = useState("");
+  const [status, setStatus] = useState("");
+  const [priority, setPriority] = useState("");
+  const [createdDate, setCreatedDate] = useState("");
+  const [dueDate, setDueDate] = useState("");
+
+  const database = props.database;
+  const setDatabase = props.setDatabase;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    /*
+    const newTask = {
+      id,
+      title,
+      description,
+      assignee,
+      status,
+      priority,
+      createdDate,
+      dueDate,
+    };
+
+    setDatabase([newTask, ...database]);
+    console.log(database);
+
+    setId("");
+    setTitle("");
+    setDescription("");
+    setAssignee("");
+    setStatus("");
+    setPriority("");
+    setCreatedDate("");
+    setDueDate("");
+    */
+  };
+
+  return (
+    <div id="edit-form-container">
+      <form onSubmit={handleSubmit} className="edit-form">
+        <h3>Create new Task</h3>
         <div>
-            <p>Bananne</p>
+          <label>
+            ID
+            <input
+              value={id}
+              name="taskID"
+              type="text"
+              placeholder="Task ID"
+              onChange={(event) => {
+                setId(event.target.value);
+              }}
+            ></input>
+          </label>
+          <label>
+            Task Title
+            <input
+              value={title}
+              name="taskTitle"
+              type="text"
+              placeholder="Task Title"
+              onChange={(event) => {
+                setTitle(event.target.value);
+              }}
+            ></input>
+          </label>
+          <label>
+            Description
+            <input
+              value={description}
+              name="taskDescription"
+              type="text"
+              placeholder="Task Description"
+              onChange={(event) => {
+                setDescription(event.target.value);
+              }}
+            ></input>
+          </label>
+          <br />
+          <label>
+            Assignee
+            <input
+              value={assignee}
+              name="taskAssignee"
+              type="text"
+              placeholder="Assignee"
+              onChange={(event) => {
+                setAssignee(event.target.value);
+              }}
+            ></input>
+          </label>
+          <label>
+            Status
+            <select
+              value={status}
+              name="taskStatus"
+              type="text"
+              placeholder="Status"
+              onChange={(event) => {
+                setStatus(event.target.value);
+              }}
+            >
+              <option value="">-- None --</option>
+              <option value="To Do">To Do</option>
+              <option value="In Progress">In Progress</option>
+              <option value="Done">Done</option>
+            </select>
+          </label>
+          <label>
+            Priority
+            <select
+              value={priority}
+              name="taskPriority"
+              type="date"
+              placeholder="Priority"
+              onChange={(event) => {
+                setPriority(event.target.value);
+              }}
+            >
+              <option value="">-- None --</option>
+              <option value="High">High</option>
+              <option value="Medium">Medium</option>
+              <option value="Low">Low</option>
+            </select>
+          </label>
+          <br />
+          <label>
+            Created
+            <input
+              value={createdDate}
+              name="taskCreationDate"
+              type="date"
+              placeholder="Created On"
+              onChange={(event) => {
+                setCreatedDate(event.target.value);
+              }}
+            ></input>
+          </label>
+          <label>
+            Due Date
+            <input
+              value={dueDate}
+              name="taskDueDate"
+              type="date"
+              placeholder="Due by"
+              onChange={(event) => {
+                setDueDate(event.target.value);
+              }}
+            ></input>
+          </label>
+          <button type="submit">Create New Task</button>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
 
-export default EditPage;
+export default EditTaskPage;
